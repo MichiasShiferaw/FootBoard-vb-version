4244797e24ea904c9bddfec84099ab865508fdd1
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-17 22:17:22 -0500

    Update footboard-vb.vb

--- footboard-vb.vb
+++ footboard-vb.vb
@@ -2,3 +2,19 @@
 Imports System
 
 Public Class FootBoardDim
+  
+End Sub
+
+
+Private Sub btnOperate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOperate.Click
+
+        'Declare all public variables
+        Dim length As Double
+        Dim width As Double
+        Dim height As Double
+
+        length = InputBox("Enter the length of the board foot (inches):")
+
+        width = InputBox("Enter the width of the board foot (inches):")
+
+
